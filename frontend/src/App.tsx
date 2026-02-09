95026df2abf66af2a10a3c550b13cae609ec536e
Lukas van Dee <lukas.van.dee@gmail.com>
2026-02-09 22:17:12 +0100

    testing auto deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Fruit React App</h1>
+        <h1>Test React App</h1>
       </header>
       <main>
         <FruitList />
