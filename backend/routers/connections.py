3f7f2e8baa68621c913c89e6936054966da3c40d
sybren <s.c.m.vandermeijden@students.uu.nl>
2026-02-14 12:09:30 +0100

    changed structure such that schema's now have their own folder

--- backend/routers/connections.py
+++ backend/routers/connections.py
@@ -5,35 +5,19 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import List
 from backend.utils.ricgraph.connections import fetch_collaborators
+from backend.schemas import Person, Publication, Organization, Connections
+
 
 # these endpoints can be reached using the /connections URL prefix
 router = APIRouter(prefix = "/connections")
 
-# a person is represented as both a name and an identifier because there can be multiple valid variations of the same name
-class Person(BaseModel):
-    author_id: str
-    name: str
-
-class Publication(BaseModel):
-    doi: str
-    title: str
-    publication_rootid: str
-    year: int
-    category: str
-    name: str
-
-class Organization(BaseModel):
-    organization_id: str
-
-class Connections(BaseModel):
-    persons: List[Person]
-    publications: List[Publication]
-    organizations: List[Organization]
-
-
 @router.get("/person/{author_id}", response_model=Connections)
 def get_person_connections(author_id: str):
-    return fetch_collaborators(author_id)
+    return Connections(
+        persons=[],
+        publications=[],
+        organizations=[]
+    )
 
 @router.get("/organization/{organization_id}", response_model=Connections)
 def get_person_publication_connections(organization_id: str):
